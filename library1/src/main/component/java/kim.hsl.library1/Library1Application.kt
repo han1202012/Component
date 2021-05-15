c3f47ff2074045d0daf316937be03b3e7b8ab349
han1202012 <octopus_truth@163.com>
2021-05-15 23:15:28 +0800

    update

---
+++ library1/src/main/component/java/kim.hsl.library1/Library1Application.kt
@@ -0,0 +1,9 @@
+package kim.hsl.library1
+
+import android.app.Application
+
+class Library1Application: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- library1/src/main/java/kim/hsl/library1/MainActivity.kt
+++ library1/src/main/java/kim/hsl/library1/MainActivity.kt
@@ -2,10 +2,13 @@ package kim.hsl.library1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i("MainActivity", "当前的模式状态 ${BuildConfig.isModuleMode}")
     }
 }
