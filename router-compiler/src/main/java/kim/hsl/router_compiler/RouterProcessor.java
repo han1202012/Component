5c573aa6877cb3d0b7557cd3604a1b225717b737
han1202012 <octopus_truth@163.com>
2021-05-22 19:40:32 +0800

    update

--- router-compiler/src/main/java/kim/hsl/router_compiler/RouterProcessor.java
+++ router-compiler/src/main/java/kim/hsl/router_compiler/RouterProcessor.java
@@ -1,10 +1,15 @@
 package kim.hsl.router_compiler;
 
 import com.google.auto.service.AutoService;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
 
+import java.io.IOException;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
 import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.Processor;
@@ -12,6 +17,7 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
 
@@ -29,6 +35,11 @@ public class RouterProcessor extends AbstractProcessor {
     private Messager mMessager;
 
     /**
+     * 用于写出生成的 Java 代码
+     */
+    private Filer mFiler;
+
+    /**
      * 该函数在初始化时调用 , 相当于构造函数
      * @param processingEnvironment
      */
@@ -36,8 +47,10 @@ public class RouterProcessor extends AbstractProcessor {
     public synchronized void init(ProcessingEnvironment processingEnvironment) {
         super.init(processingEnvironment);
         // 获取打印日志接口
-        mMessager = processingEnvironment.getMessager();
+        this.mMessager = processingEnvironment.getMessager();
         mMessager.printMessage(Diagnostic.Kind.NOTE, "Messager Print Log");
+
+        this.mFiler = processingEnvironment.getFiler();
     }
 
     /**
@@ -53,6 +66,30 @@ public class RouterProcessor extends AbstractProcessor {
     public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
         for (TypeElement typeElement: set){
             mMessager.printMessage(Diagnostic.Kind.NOTE, "SupportedAnnotationTypes : " + typeElement.getQualifiedName());
+
+            // 生成 public static void main(String[] args) 函数
+            MethodSpec main = MethodSpec.methodBuilder("main")
+                    .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                    .returns(void.class)
+                    .addParameter(String[].class, "args")
+                    .addStatement("$T.out.println($S)", System.class, "Hello, JavaPoet!")
+                    .build();
+
+            // 指定 public final class HelloWorld 类
+            TypeSpec helloWorld = TypeSpec.classBuilder("HelloWorld")
+                    .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                    .addMethod(main)
+                    .build();
+
+            // 正式在 "com.example.helloworld" 包名下创建 HelloWorld 类
+            JavaFile javaFile = JavaFile.builder("com.example.helloworld", helloWorld)
+                    .build();
+
+            try {
+                javaFile.writeTo(mFiler);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
         }
         return false;
